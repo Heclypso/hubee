272332ca94e672c142341567215c427223c16efb
Heclypso <heclypso@gmail.com>
2025-05-27 14:20:22 -0300

    #1 feat: desenvolvido navbar e hero

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,23 @@
+import '../../global.css';
+import styles from './Hero.module.css';
+
+const Hero = () => {
+
+    return (
+        <div className={styles.hero}>
+            <div className="container">
+                <div className={styles.container__left}>
+                    <h1 className={styles.hero__title}>Móveis usados</h1>
+                    <h2 className={styles.hero__subtitle}>Móveis semi-novos de qualidade e por um preço acessível.</h2>
+                </div>
+                <div className={styles.container__right}>
+                    <img className={styles.container__right__image} src="https://placehold.co/100x244" alt="" />
+                    <img className={styles.container__right__image} src="https://placehold.co/100x300" alt="" />
+                    <img className={styles.container__right__image} src="https://placehold.co/100x244" alt="" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
