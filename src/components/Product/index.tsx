5a2a0cbfefd65c655d75c182e42c888ba128b0c3
Heclypso <heclypso@gmail.com>
2025-05-29 21:57:40 -0300

    #4 feat: desenvolvido os temas das tags dos produtos

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -7,7 +7,17 @@ const Product = (): React.JSX.Element => {
             <img className={styles.product__image} src="https://placehold.co/300x220" alt="" />
             <div className={styles.product__infos}>
                 <h2 className={styles.product__title}>Sofá reclinável 3 lugares</h2>
-                <span className={styles.product__tag}>Semi-novo</span>
+                <div className={styles['product__tag-wrapper']}>
+                    <span className={styles.product__tag}>Semi-novo</span>
+                    <span className={styles.product__tag}>Cubro folga</span>
+                    <span className={`${styles.product__tag} ${styles['product__tag--purple']}`}>Diária</span>
+                    <span className={`${styles.product__tag} ${styles['product__tag--purple']}`}>P/Hora</span>
+                    <span className={`${styles.product__tag} ${styles['product__tag--green']}`}>P/Unidade</span>
+                    <span className={`${styles.product__tag} ${styles['product__tag--green']}`}>P/Kg</span>
+                    <span className={`${styles.product__tag} ${styles['product__tag--green']}`}>P/Cento</span>
+                    <span className={`${styles.product__tag} ${styles['product__tag--ran-out']}`}>Acabou</span>
+                    <span className={`${styles.product__tag} ${styles['product__tag--pink']}`}>P/Serviço</span>
+                </div>
                 <p className={styles['product__total-price']}>1x de R$ 157</p>
                 <p className={styles.product__installment}>Em até 3x sem juros de R$ 52,33 </p>
             </div>
